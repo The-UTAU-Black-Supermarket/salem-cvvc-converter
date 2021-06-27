fa35004b59fccc252faf79f8aa068f4e712d9675
Mark Beltran <markmbeltran@gmail.com>
2021-06-27 10:06:17 +0800

    Basic plugin GUI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using Xceed.Wpf.Toolkit;
+using utauPlugin;
+
+namespace LengthRandomizer
+{
+    public partial class MainWindow : Window
+    {
+        private UtauPlugin utauPlugin;
+        string[] args = Environment.GetCommandLineArgs();
+        public MainWindow()
+        {
+            InitializeComponent();
+            utauPlugin = new UtauPlugin(args[1]);
+            utauPlugin.Input();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OK(object sender, RoutedEventArgs e)
+        {
+            foreach (Note note in utauPlugin.note)
+            {
+                // do something
+
+                //note.SetLength(note.GetLength() + rand.Next(0 - (int)this.amount.Value, (int)this.amount.Value));
+            }
+            utauPlugin.Output();
+            Close();
+        }
+    }
+}
